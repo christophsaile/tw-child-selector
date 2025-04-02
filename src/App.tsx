ff0ffc7905bad58eb37ac8049d495011448b3562
Christoph Saile <christoph@alm.sh>
2025-04-02 13:10:30 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <ul className="bg-red-500 *:custom-class">
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+    </ul>
+  );
+}
+
+export default App;
